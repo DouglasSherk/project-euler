9e2f05029c824232e2b9029aad3d9a31af1b0c04
Douglas Sherk <github@sherk.me>
2017-06-01 17:20:06 -0400

    Working but very slow problem 60

--- 60.cpp
+++ 60.cpp
@@ -1,55 +1,61 @@
+/**
+ * WARNING: This currently takes several minutes to run to completion. I'm not
+ * sure why it's so much slower than other published answers as it uses the same
+ * principles.
+ */
+
 #include <iostream>
 #include <set>
+#include <iterator>
 #include <unordered_map>
+#include <stdlib.h>     /* srand, rand */
+#include <time.h>       /* time */
+#include <climits>
 
 #include "headers/euler.hpp"
 
-const int NUM_PRIME_COMBINATIONS = 5;
-const int MAX_PRIME = 30000;
-
 typedef unsigned long long prime_value;
 typedef std::set<prime_value> prime_container;
+typedef prime_container::iterator prime_iterator;
+typedef std::vector<prime_iterator> prime_iterator_container;
 typedef std::unordered_map<size_t, bool> prime_pairs_container;
+
+const int NUM_PRIME_COMBINATIONS = 5;
+const int MAX_PRIME_GENERATE = 30000;
+const prime_value PRIME_VALUE_MAX = ULLONG_MAX;
+
+prime_pairs_container primePairs;
 
 size_t prime_value_key(prime_value a, prime_value b) {
   return (size_t) a << 32 | (prime_value) b;
 }
 
-prime_value concatPrimes(prime_value a, prime_value b) {
-  prime_value times = 1;
-  while (times <= b) {
-    times *= 10;
-  }
-  return a * times + b;
-}
+bool areConcatsPrime(prime_value a, prime_value b) {
+  size_t primeValueKey = prime_value_key(a, b);
 
-bool areConcatsPrime(prime_value a, prime_value b,
-                     const prime_container& primes,
-                     prime_pairs_container& primePairs) {
-  // std::cout << "evaluating " << a << ", " << b << std::endl;
-  if (primePairs.find(prime_value_key(a, b)) != primePairs.end()) {
-    return primePairs[prime_value_key(a, b)];
+  if (primePairs.find(primeValueKey) != primePairs.end()) {
+    return primePairs[primeValueKey];
   }
 
-  bool isConcatPrime = primes.find(
-    // euler::toInt(std::to_string(a) + std::to_string(b))
-    concatPrimes(a, b)
-  ) != primes.end() && primes.find(
-    // euler::toInt(std::to_string(b) + std::to_string(a))
-    concatPrimes(b, a)
-  ) != primes.end();
+  bool isConcatPrime = euler::isPrime(
+    euler::toInt(std::to_string(a) + std::to_string(b))
+  ) && euler::isPrime(
+    euler::toInt(std::to_string(b) + std::to_string(a))
+  );
 
-  primePairs[prime_value_key(a, b)] = isConcatPrime;
+  primePairs[primeValueKey] = isConcatPrime;
 
   return isConcatPrime;
 }
 
-bool allConcatsArePrime(prime_value* primeSet,
-                        const prime_container& primes,
-                        prime_pairs_container& primePairs) {
-  for (int i = 0; i < NUM_PRIME_COMBINATIONS - 1; i++) {
-    for (int j = i + 1; j < NUM_PRIME_COMBINATIONS; j++) {
-      if (!areConcatsPrime(primeSet[i], primeSet[j], primes, primePairs)) {
+bool allConcatsArePrime(const std::vector<prime_iterator>& primeIts) {
+  if (primeIts.empty()) {
+    return false;
+  }
+
+  for (auto i = primeIts.begin(); i != primeIts.end(); i++) {
+    for (auto j = i + 1; j != primeIts.end(); j++) {
+      if (!areConcatsPrime(**i, **j)) {
         return false;
       }
     }
@@ -58,30 +64,62 @@ bool allConcatsArePrime(prime_value* primeSet,
   return true;
 }
 
+void advanceIterators(prime_iterator_container& primeIters,
+                      const prime_container& primes) {
+  while (!primeIters.empty() && ++primeIters.back() == primes.end()) {
+    primeIters.pop_back();
+  }
+}
+
 int main(int argc, char** argv) {
-  std::cout << "Allocating primes" << std::endl;
-  auto primes(euler::generatePrimes<prime_container>(MAX_PRIME));
-  std::cout << "Allocated primes" << std::endl;
+  srand(time(NULL));
 
-  std::vector<prime_value> primesList(primes.begin(), primes.end());
-  prime_pairs_container primePairs;
+  auto primes(euler::generatePrimes<prime_container>(MAX_PRIME_GENERATE));
 
-  for (int a = 0; a < primesList.size(); a++) {
-    std::cout << "evaluating new a: " << primesList[a] << std::endl;
-    for (int b = a + 1; b < primesList.size(); b++) {
-      for (int c = b + 1; c < primesList.size(); c++) {
-        for (int d = c + 1; d < primesList.size(); d++) {
-          for (int e = d + 1; e < primesList.size(); e++) {
-            prime_value primeSet[NUM_PRIME_COMBINATIONS] = {primesList[a], primesList[b], primesList[c], primesList[d], primesList[e]};
-            if (allConcatsArePrime(primeSet, primes, primePairs)) {
-              std::cout << "Found a series of concated primes: " << std::endl;
-              std::cout << a << ", " << b << ", " << c << ", " << d << ", " << e << std::endl;
-              break;
-            }
-          }
-        }
-      }
+  prime_iterator_container primeIters;
+  primeIters.push_back(primes.begin());
+  prime_value result = PRIME_VALUE_MAX;
+
+  while (!primeIters.empty()) {
+    prime_value sum = 0;
+    for (auto it : primeIters) {
+      sum += *it;
     }
+    if (sum > result) {
+      primeIters.pop_back();
+      advanceIterators(primeIters, primes);
+    }
+
+    prime_iterator iter = primeIters.back();
+
+    prime_iterator_container possiblePrimeIters;
+
+    while (!allConcatsArePrime(possiblePrimeIters) && ++iter != primes.end()) {
+      possiblePrimeIters = primeIters;
+      possiblePrimeIters.push_back(iter);
+    }
+
+    if (iter != primes.end()) {
+      primeIters.push_back(iter);
+
+      if (primeIters.size() >= NUM_PRIME_COMBINATIONS) {
+        sum += *iter;
+        if (sum < result) {
+          result = sum;
+        }
+        std::cout << "Found a chain of sum " << sum << ": ";
+        for (auto primeIter : primeIters) {
+          std::cout << *primeIter << " ";
+        }
+        std::cout << std::endl;
+
+        advanceIterators(primeIters, primes);
+      }
+
+      continue;
+    }
+
+    advanceIterators(primeIters, primes);
   }
 
   return 0;
